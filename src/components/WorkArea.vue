e1722830a20da1068899af3c791d7fbcaa942a5d
NNN <b7080@oic.jp>
2019-03-28 23:47:11 +0900

    add 選択肢3存在時の描画を設定

--- src/components/WorkArea.vue
+++ src/components/WorkArea.vue
@@ -4,8 +4,8 @@
 			<v-layout row wrap>
 				<v-flex xs12>
 					<v-card>
-						<v-text-field label="診断名" v-model="judgement.judgement_name"></v-text-field>
-						<v-text-field label="質問文" v-model="judgement.conditions_branch"></v-text-field>
+						<v-text-field label="診断名" v-model="judgement.judgement_name" @change="generate()"></v-text-field>
+						<v-text-field label="質問文" v-model="judgement.conditions_branch" @change="generate()"></v-text-field>
 						<v-select
 							:items="[2,3]"
 							v-model="judgement.branch_count"
@@ -18,33 +18,34 @@
 				<v-flex v-bind:class="branch_quantity.layout">
 					<v-card>
 						<v-text-field label="選択肢1" v-model="branches.branch1.display_text" @change="generate()"></v-text-field>
-						<v-text-field label="選択肢1の結果" v-model="branches.branch1.result"></v-text-field>
+						<v-text-field label="あなたは..." v-model="branches.branch1.result" @change="generate()"></v-text-field>
 					</v-card>
 				</v-flex>
 				<v-flex v-bind:class="branch_quantity.layout">
 					<v-card>
-						<v-text-field label="選択肢2" v-model="branches.branch2.display_text"></v-text-field>
-						<v-text-field label="選択肢2の結果" v-model="branches.branch2.result"></v-text-field>
+						<v-text-field label="選択肢2" v-model="branches.branch2.display_text" @change="generate()"></v-text-field>
+						<v-text-field label="あなたは..." v-model="branches.branch2.result" @change="generate()"></v-text-field>
 					</v-card>
 				</v-flex>
 				<v-flex v-if="branches.branch3.exists" v-bind:class="branch_quantity.layout">
 					<v-card>
-						<v-text-field label="選択肢3" v-model="branches.branch3.display_text"></v-text-field>
-						<v-text-field label="選択肢3の結果" v-model="branches.branch3.result"></v-text-field>
+						<v-text-field label="選択肢3" v-model="branches.branch3.display_text" @change="generate()"></v-text-field>
+						<v-text-field label="あなたは..." v-model="branches.branch3.result" @change="generate()"></v-text-field>
 					</v-card>
 				</v-flex>
 			</v-layout>
 		</v-container>
 		<v-container>
-			<div class="row mb-2 mr-1 mr-1 ml-1">
-        <button type="button" class="col-md-6 btn btn-success" @click="generate">嵐を呼ぶ！</button>
+			<!-- <div class="row mb-2 mr-1 mr-1 ml-1">
         <a href ="https://twitter.com/intent/tweet?&hashtags=嵐を呼ぶジェネレーター&url=https://afterschoolstudy.github.io/generator_of_call_a_storm" class="col-md-6 btn btn-primary">嵐をシェアする！</a>
-      </div>
+      </div> -->
 
-      <div class="mb-4">
-        <canvas id="output_field" width="2880" height="1620" v-show="!isGenerated"></canvas>
-        <img class="output_image" :src="src" v-show="isGenerated" />
-      </div>
+      <v-layout row wrap>
+				<v-flex xs12>
+					<canvas id="output_field" width="2880" height="1620" v-show="!isGenerated"></canvas>
+					<img class="output_image" :src="src" v-show="isGenerated" />
+				</v-flex>
+      </v-layout>
 		</v-container>
 	</div>
 </template>
@@ -54,8 +55,8 @@ export default {
 	data(){
 		return {
 			judgement:{
-				judgement_name: '',
-				conditions_branch: '',
+				judgement_name: '5秒でわかる診断',
+				conditions_branch: 'きのこ派？たけのこ派？',
 				branch_count: 2,
 			},
 			branches: {
@@ -77,8 +78,21 @@ export default {
 				layout: 'sm6'
 			},
 			src: '',
-			isGenerated : false
+			isGenerated : false,
+			drawPoint: {
+				xCenter: 1440,
+				yCenter: 810,
+				conditionCenter: 500,
+				diaWidth: 720,
+				branchBar: 200,
+				branchHeight: 150,
+				resultHeight: 1300,
+				resultBoxHeight: 250
+			}
 		}
+	},
+	mounted(){
+		this.generate()
 	},
 	methods:{
 		branch_count_change(){
@@ -89,12 +103,17 @@ export default {
 				this.branch_quantity.layout = 'sm6'
 				this.branches.branch3.exists = false
 			}
+			this.generate()
 		},
 		generate () {
       // 描画
-      let ctx = document.getElementById('output_field').getContext('2d')
-      this.drawBg(ctx, '桃 ~ 黃')
-      this.drawTitle(ctx, '桃 ~ 黃')
+			let ctx = document.getElementById('output_field').getContext('2d')
+			this.drawInitial(ctx)
+			this.drawTitle(ctx)
+			if(this.branches.branch3.exists){
+				// 第3の選択肢.表示ありなら
+				this.drawBranch3(ctx)
+			}
       this.drawCaption(ctx, this.branches.branch1.display_text, 'left', { 'x': 400, 'y': 1350 })
       this.drawCaption(ctx, 'iiii', 'right', { 'x': 2480, 'y': 1350 })
       // 生成
@@ -102,13 +121,14 @@ export default {
       this.isGenerated = true
       this.src = ctx.toDataURL()
     },
-    drawTitle (ctx, colorType) {
-      ctx.font = '250px sans-serif'
-      ctx.textAlign = 'left'
-      ctx.fillStyle = colorType === '桃 ~ 黃' ? '#5853A0' : '#633EBF'
-      ctx.fillText('bbbb', 140, 590)
-      ctx.textAlign = 'right'
-      ctx.fillText('cccc', 2740, 930)
+    drawTitle (ctx) {
+			// 診断タイトルと条件式を描画
+      ctx.font = '120px sans-serif'
+      ctx.textAlign = 'center'
+      ctx.fillStyle = '#000'
+			ctx.fillText(this.judgement.judgement_name, this.drawPoint.xCenter, 200)
+			ctx.font = '75px sans-serif'
+      ctx.fillText(this.judgement.conditions_branch, this.drawPoint.xCenter, this.drawPoint.conditionCenter)
     },
     drawCaption (ctx, text, align, coordinate) {
       ctx.textAlign = align
@@ -118,21 +138,52 @@ export default {
       ctx.fillText(text, coordinate['x'], coordinate['y'])
       ctx.strokeStyle = 'gray'
       ctx.strokeText(text, coordinate['x'], coordinate['y'])
-    },
-    drawBg (ctx, colorType) {
-      ctx.beginPath()
-      const ctxBaseColor = ctx.createLinearGradient(0, 410, 0, 1620)
-      if (colorType === '桃 ~ 黃') {
-        ctxBaseColor.addColorStop(0.0, '#FD81A6')
-        ctxBaseColor.addColorStop(1.0, '#FFFF83')
-      } else {
-        ctxBaseColor.addColorStop(0.0, '#AEEC97')
-        ctxBaseColor.addColorStop(0.3, '#FFF')
-        ctxBaseColor.addColorStop(1.0, '#73B2F9')
-      }
-      ctx.fillStyle = ctxBaseColor
-      ctx.fillRect(0, 0, 2880, 1620)
-    }
+		},
+		resetCanvas(ctx){
+			ctx.fillStyle = '#fff'
+			ctx.fillRect(0, 0, 2880, 1620)
+		},
+		drawInitial (ctx) {
+			// 最初のひし形書く
+			this.resetCanvas(ctx)
+			ctx.beginPath()
+			ctx.strokeStyle = '#000'
+			ctx.lineWidth = '5'
+			ctx.moveTo(this.drawPoint.xCenter, this.drawPoint.conditionCenter-200)
+			ctx.lineTo(this.drawPoint.xCenter+this.drawPoint.diaWidth, this.drawPoint.conditionCenter-25)
+			ctx.lineTo(this.drawPoint.xCenter, this.drawPoint.conditionCenter+175)
+			ctx.lineTo(this.drawPoint.xCenter-this.drawPoint.diaWidth, this.drawPoint.conditionCenter-25)
+			ctx.closePath()
+			ctx.stroke()
+		},
+		drawBranch3(ctx){
+			ctx.beginPath();
+			ctx.strokeStyle = '#000'
+			ctx.lineWidth = '5'
+			ctx.moveTo(this.drawPoint.xCenter, this.drawPoint.conditionCenter+175)
+			ctx.lineTo(this.drawPoint.xCenter, this.drawPoint.conditionCenter+175 + this.drawPoint.branchBar)
+			ctx.closePath()
+			ctx.moveTo(1080, this.drawPoint.conditionCenter+175 + this.drawPoint.branchBar)
+			ctx.lineTo(1800, this.drawPoint.conditionCenter+175 + this.drawPoint.branchBar)
+			ctx.lineTo(1800, this.drawPoint.conditionCenter+175 + this.drawPoint.branchBar+this.drawPoint.branchHeight)
+			ctx.lineTo(1080, this.drawPoint.conditionCenter+175 + this.drawPoint.branchBar+this.drawPoint.branchHeight)
+			ctx.closePath()
+			ctx.font = '110px sans-serif'
+      ctx.textAlign = 'center'
+			ctx.fillStyle = '#000'
+			ctx.fillText(this.branches.branch3.display_text, this.drawPoint.xCenter, this.drawPoint.conditionCenter+150 + this.drawPoint.branchBar+this.drawPoint.branchHeight)
+			ctx.moveTo(this.drawPoint.xCenter, this.drawPoint.conditionCenter+175 + this.drawPoint.branchBar+this.drawPoint.branchHeight)
+			ctx.lineTo(this.drawPoint.xCenter, this.drawPoint.resultHeight)
+			ctx.moveTo(1080, this.drawPoint.resultHeight)
+			ctx.lineTo(1800, this.drawPoint.resultHeight)
+			ctx.lineTo(1800,this.drawPoint.resultHeight+this.drawPoint.resultBoxHeight)
+			ctx.lineTo(1080,this.drawPoint.resultHeight+this.drawPoint.resultBoxHeight)
+			ctx.closePath()
+			ctx.font = '80px sans-serif'
+			ctx.fillText('あなたは', this.drawPoint.xCenter, this.drawPoint.resultHeight+115)
+			ctx.fillText(this.branches.branch3.result+'です', this.drawPoint.xCenter, this.drawPoint.resultHeight+190)
+			ctx.stroke()
+		}
 	}
 }
 </script>
